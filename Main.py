5cce37dd7cc0f45b62ce168d81d2333ee6e5149a
Yakudzat <ivannabaikale@gmail.com>
2025-12-12 21:18:38 +0800

    second  commit

--- Main.py
+++ Main.py
@@ -1,1 +1,22 @@
-print("Hello World")
+class Owner:
+
+    class Pet:
+        def __init__(self, name, kind, mood, hunger):
+            self.name = name
+            self.kind = kind
+            self.__mood = mood # Настроение
+            self.__hunger = hunger # голод
+
+        def feed(self):
+            self.__mood += 10
+            self.__hunger -= 10
+
+        def play(self):
+            self.__mood += 10
+            self.__hunger += 10
+
+        def speak(self):
+            return ""
+
+
+
